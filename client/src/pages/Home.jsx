4ec863664b5d55555a5018809e620390e6ba394a
sriyan <anuruddasriyan@gmail.com>
2024-03-10 00:56:39 +0530

    show alert to user of token is available

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,11 +5,29 @@ import PlayGameButton from "../components/PlayGameButton"
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/AuthContext";
+
 
 export default function Home() {
+    const [showAlert, setShowAlert] = useState(true);
+
+    const { authUser } = useContext(AuthContext)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowAlert(false);
+        }, 5000); // close Alert after 5 seconds 
+
+        return () => clearTimeout(timer);
+    }, []);
+
+
     return (
         <div>
             <NavigationBar />
+            {showAlert && authUser.isAuthenticated && <Alert variant="success" >Welcome, {authUser.username}! </Alert >}
             <div className="p-5 container">
                 <div className="row">
                     <div className="col-sm">
@@ -18,11 +36,14 @@ export default function Home() {
                     <div className="col-sm d-flex justify-content-center">
                         <PlayGameButton />
                     </div>
-                    <div className="col-sm">
-                        <TabPanel />
-                    </div>
+                    {
+                        !authUser.isAuthenticated &&
+                        <div className="col-sm">
+                            <TabPanel />
+                        </div>
+                    }
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
